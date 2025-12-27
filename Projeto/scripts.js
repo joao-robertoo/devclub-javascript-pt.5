7f77a69220145240dece4dacc446a34b5b681f1d
J <joao.adsbrasil@gmail.com>
2025-12-26 23:30:30 -0300

    Projeto - FILTER

--- Projeto/scripts.js
+++ Projeto/scripts.js
@@ -2,6 +2,15 @@ const list = document.querySelector("ul")
 const buttonShowAll = document.querySelector(".show-all")
 const buttonMapAll = document.querySelector(".map-all")
 const sumAll = document.querySelector(".sum-all")
+const filterAll = document.querySelector(".filter-all")
+
+function formatCurrency(value) {
+    const newValue =value.toLocaleString('pt-br', {
+        style: 'currency',
+        currency: 'BRL'
+    })
+    return newValue
+}
 
 function showAll(productsArray) {
     let myLi = ''
@@ -10,7 +19,7 @@ function showAll(productsArray) {
             <li>
                 <img src="${product.src}">
                 <p>${product.name}</p>
-                <p class="item-price">R$ ${product.price}</p>
+                <p class="item-price"> ${formatCurrency(product.price)}</p>
             </li>
            `
     })
@@ -30,12 +39,19 @@ function sumAllItems() {
     const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
     list.innerHTML = `
                         <li>
-                            <p>O valor total dos itens é R$ ${totalValue}</p>
+                            <p>O valor total dos itens é ${formatCurrency(totalValue)}</p>
                         </li>
                 `
+}
+
+function filterAllItems() {
+    const filterJustVegan = menuOptions.filter((product) => product.vegan)
+
+    showAll(filterJustVegan)
 }
 
 buttonShowAll.addEventListener("click", () => showAll(menuOptions))
 buttonMapAll.addEventListener("click", mapAllItems)
 sumAll.addEventListener("click", sumAllItems)
+filterAll.addEventListener("click", filterAllItems)
 
